fbb53b026d456cad713b09889b5ddce2e04eaf7a
sahuswastik <100347441+sahuswastik@users.noreply.github.com>
2022-06-25 13:45:15 +0530

    working on top-bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -5,7 +5,9 @@ export default function Topbar() {
     <div className="topbar">
       <div className="wrapper">
         <div className="left">
-          <a href="#intro">asdsad</a>
+          <a href="#intro" className="logo">
+            genius.
+          </a>
         </div>
         <div className="right"></div>
       </div>
