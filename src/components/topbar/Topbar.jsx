1326ce37f2ce8cc153a2c17120ae0446286085cf
sahuswastik <100347441+sahuswastik@users.noreply.github.com>
2022-06-25 12:49:25 +0530

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,14 @@
+import "./topbar.scss";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">asdsad</a>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+}
