7e8d7b28d0ecd89a3d12ee25173743fbe9cc6968
sahuswastik <100347441+sahuswastik@users.noreply.github.com>
2022-06-27 11:15:42 +0530

    completed contact page

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,44 @@
 import "./topbar.scss";
+import { Person, Mail } from "@mui/icons-material";
+import { ReactComponent as Git } from "../../../public/assets/github-brands.svg";
+import { ReactComponent as LinkedIn } from "../../../public/assets/linkedin-brands.svg";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
-            genius.
+            portfolio
           </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+91 7008913909</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>sahuswastik30@gmail.com</span>
+          </div>
+          <div className="itemContainer">
+            <a href="https://github.com/sahuswastik">
+              <Git />
+              <span>Git</span>
+            </a>
+          </div>
+          <div className="itemContainer">
+            <a href="https://www.linkedin.com/">
+              <LinkedIn className="icon" />
+              <span>Link</span>
+            </a>
+          </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,5 +1,78 @@
+import { useState } from "react";
 import "./works.scss";
 
 export default function Works() {
-  return <div className="works" id="works"></div>;
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const data = [
+    {
+      id: "1",
+      icon: "./assets/mobile.png",
+      title: "Crown Clothing",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      img: "https://preply.com/wp-content/uploads/2018/04/shopping_bags.jpg",
+      link: "https://glittery-granita-256a46.netlify.app/",
+    },
+    {
+      id: "2",
+      icon: "./assets/globe.png",
+      title: "Face Detection",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://newsroom.unsw.edu.au/sites/default/files/thumbnails/image/facerecognition_1.jpg",
+    },
+    {
+      id: "3",
+      icon: "./assets/writing.png",
+      title: "Robo-Friends",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://previews.123rf.com/images/scottff72/scottff721401/scottff72140100010/24802604-a-robot-woman-head-with-internal-technology-side-view-isolated-on-white-background.jpg",
+    },
+  ];
+
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+
+  return (
+    <div className="works" id="works">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={d.icon} alt="" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
+                  {/* <span>Projects</span> */}
+                  <a href={d.link}>Project</a>
+                </div>
+              </div>
+              <div className="right">
+                <img src={d.img} alt="" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <img
+        src="assets/arrow.png"
+        className="arrow left"
+        alt=""
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src="assets/arrow.png"
+        className="arrow right"
+        alt=""
+        onClick={() => handleClick()}
+      />
+    </div>
+  );
 }
